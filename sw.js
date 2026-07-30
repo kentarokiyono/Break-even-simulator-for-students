5d382db81ba7dafe1fce9410cd897c45b7c71fc6
kentarō kiyono <90179267+kentarokiyono@users.noreply.github.com>
2026-07-30 14:26:04 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "bev-v1";
+const CACHE = "bev-v2";
 const ASSETS = ["./", "./index.html", "./manifest.json"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(() => {}));
